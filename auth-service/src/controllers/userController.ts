21bc817a528d824b2e1626e4612eb25fb17c535e
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-08-23 19:44:06 +0100

    Created Error handler

--- auth-service/src/controllers/userController.ts
+++ auth-service/src/controllers/userController.ts
@@ -1,23 +1,24 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/RequestValidationError';
+import { DatabaseConnectionError } from '../errors/DatabaseConnectionError';
 
 class UserController {
   static async getCurrentUser(req: Request, res: Response) {
     res.send('Testing Get User');
   };
 
-  static async createUser(req: Request, res: Response) {
+  static createUser(req: Request, res: Response) {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      const error = new Error('Invalid email or password');
-      
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     console.log('Creating a user...');
+    throw new DatabaseConnectionError();
 
     res.send({});
   }
--- auth-service/src/middlewares/error-handler.ts
+++ auth-service/src/middlewares/error-handler.ts
@@ -1,5 +1,20 @@
 import { Request, Response, NextFunction} from 'express';
+import { RequestValidationError } from '../errors/RequestValidationError';
+import { DatabaseConnectionError } from '../errors/DatabaseConnectionError';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param }
+    });
 
+    return res.status(400).json({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    const errors = [{ message: err.reason }]
+    return res.status(500).json({ errors })
+  }
+
+  res.json({ message: 'Something went wrong'});
 }
