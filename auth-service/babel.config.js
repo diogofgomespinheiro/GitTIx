c7b29598a757df576bc770aceaac5dd4273ddfbd
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-08-29 02:49:26 +0100

    setup: finish configurations

---
+++ auth-service/babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@config': './src/config',
+        '@controllers': './src/controllers',
+        '@middlewares': './src/middlewares',
+        '@models': './src/models',
+        '@routes': './src/routes'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
