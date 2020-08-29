92291495e2c2218bc41f96ea484ed910a4d6273b
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-08-29 03:13:05 +0100

    setup: added new module path for errors folder

--- auth-service/babel.config.js
+++ auth-service/babel.config.js
@@ -4,24 +4,26 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: {
-          node: 'current'
-        }
-      }
+          node: 'current',
+        },
+      },
     ],
-    '@babel/preset-typescript'
+    '@babel/preset-typescript',
   ],
   plugins: [
-    ['module-resolver', {
-      alias: {
-        '@config': './src/config',
-        '@controllers': './src/controllers',
-        '@middlewares': './src/middlewares',
-        '@models': './src/models',
-        '@routes': './src/routes'
-      }
-    }]
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@controllers': './src/controllers',
+          '@errors': './src/errors',
+          '@middlewares': './src/middlewares',
+          '@models': './src/models',
+          '@routes': './src/routes',
+        },
+      },
+    ],
   ],
-  ignore: [
-    '**/*.spec.ts'
-  ]
-}
+  ignore: ['**/*.spec.ts'],
+};
