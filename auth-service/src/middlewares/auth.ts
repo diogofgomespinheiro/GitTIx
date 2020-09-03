44baf94082ef3e0fae736c34d40623dcd4b0da15
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-09-03 01:06:49 +0100

    feat: created new auth middleware

---
+++ auth-service/src/middlewares/auth.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { JsonWebToken } from '@utils/jwt';
+import { NotAuthorizedError } from '@errors/NotAuthorizedError';
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare module 'express' {
+  export interface Request {
+    currentUser?: UserPayload;
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const currentJwt = req.session?.jwt;
+
+  if (!currentJwt) {
+    return next();
+  }
+
+  try {
+    const payload = JsonWebToken.verifyToken(currentJwt) as UserPayload;
+    req.currentUser = payload;
+  } catch (err) {}
+
+  next();
+};
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!req.currentUser) {
+    throw new NotAuthorizedError();
+  }
+
+  next();
+};
