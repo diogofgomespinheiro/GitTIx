bf86f3c504d78c5076de81c514ce799d4c48a3e7
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-09-11 13:39:53 +0100

    fix: removed classNames

--- client/components/FormInput/index.js
+++ client/components/FormInput/index.js
@@ -5,10 +5,8 @@ import { FormGroup, Label, Input } from './styles';
 const FormInput = ({ label, name, ...otherProps }, ref) => {
   return (
     <FormGroup>
-      <Label htmlFor={name} className="form-input-label">
-        {label}
-      </Label>
-      <Input className="form-input" name={name} {...otherProps} ref={ref} />
+      <Label htmlFor={name}>{label}</Label>
+      <Input name={name} {...otherProps} ref={ref} />
     </FormGroup>
   );
 };
