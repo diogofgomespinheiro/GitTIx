79f8aa570e2e49877df9dcb23268c38194440d85
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-09-12 13:37:22 +0100

    chore: add new library to load svg images as component

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  webpackDevMiddleware: config => {
+  webpack(config) {
     config.watchOptions.poll = 300;
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
     return config;
-  }
-}
+  },
+};
