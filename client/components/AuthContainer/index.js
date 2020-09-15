a3abfa067a44cbf8c98fd1c5fe2c7c81dec51301
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-09-15 20:24:55 +0100

    refactor: create new component for auth pages content

---
+++ client/components/AuthContainer/index.js
@@ -0,0 +1,56 @@
+// Library imports
+import { useRef } from 'react';
+import Router from 'next/router';
+
+// Component imports
+import FormInput from '../../components/FormInput';
+
+// Hooks imports
+import useRequest from '../../hooks/useRequest';
+
+// Styles
+import { Container, FormContainer, Title } from '../../styles/pages/Auth';
+
+const AuthContainer = ({ title, buttonText, requestUrl }) => {
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+  const { doRequest } = useRequest({
+    url: requestUrl,
+    method: 'post',
+    onSuccess: () => Router.push('/'),
+  });
+
+  const handleSubmit = async evt => {
+    evt.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    await doRequest({ email, password });
+  };
+
+  return (
+    <Container>
+      <FormContainer onSubmit={handleSubmit}>
+        <Title>{title}</Title>
+        <FormInput
+          type="email"
+          name="email"
+          ref={emailInputRef}
+          label="Email"
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          ref={passwordInputRef}
+          label="Password"
+          required
+        />
+        <button>{buttonText}</button>
+      </FormContainer>
+    </Container>
+  );
+};
+
+export default AuthContainer;
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,55 +1,12 @@
-// Library imports
-import { useRef } from 'react';
-import Router from 'next/router';
-
-// Component imports
-import FormInput from '../../components/FormInput';
-
-// Hooks imports
-import useRequest from '../../hooks/useRequest';
-
-// Styles
-import { Container, FormContainer, Title } from '../../styles/pages/Signup';
+import AuthContainer from '../../components/AuthContainer';
 
 const SignUp = () => {
-  const emailInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
-  const { doRequest } = useRequest({
-    url: '/api/users/signup',
-    method: 'post',
-    onSuccess: () => Router.push('/'),
-  });
-
-  const handleSubmit = async evt => {
-    evt.preventDefault();
-
-    const email = emailInputRef.current.value;
-    const password = passwordInputRef.current.value;
-
-    await doRequest({ email, password });
-  };
-
   return (
-    <Container>
-      <FormContainer onSubmit={handleSubmit}>
-        <Title>Sign Up</Title>
-        <FormInput
-          type="email"
-          name="email"
-          ref={emailInputRef}
-          label="Email"
-          required
-        />
-        <FormInput
-          type="password"
-          name="password"
-          ref={passwordInputRef}
-          label="Password"
-          required
-        />
-        <button>Sign Up</button>
-      </FormContainer>
-    </Container>
+    <AuthContainer
+      title="Sign Up"
+      buttonText="Sign Up"
+      requestUrl="/api/users/signup"
+    />
   );
 };
 
