34324071fe1b0e5013d09d424d0b6bec664ce5fb
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-09-27 21:23:12 +0100

    chore (payments-service): add k8s files

--- payments-service/src/config/dev.ts
+++ payments-service/src/config/dev.ts
@@ -3,7 +3,7 @@ export const config = {
     signed: false,
     secure: true,
   },
-  dbUrl: 'mongodb://payments-mongo-srv:27017/orders',
+  dbUrl: 'mongodb://payments-mongo-srv:27017/payments',
   jwtKey: process.env.JWT_KEY,
   natsClientId: process.env.NATS_CLIENT_ID,
   natsClusterId: process.env.NATS_CLUSTER_ID,
