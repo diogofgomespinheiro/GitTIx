9d364846637c4a0dbcf0e2e4c7de355c2e591656
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-03 18:41:06 +0100

    feat (client): display tickets on homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,67 @@
-const Home = ({ currentUser }) => {
-  return currentUser ? <div>Home</div> : <div>Your are signout</div>;
+//Library imports
+import Link from 'next/link';
+
+// Api/Helpers imports
+import buildClient from './api/buildClient';
+
+// Style imports
+import {
+  Button,
+  Container,
+  Table,
+  TableContainer,
+  Title,
+} from '../styles/pages/Home';
+
+const Home = ({ currentUser, tickets }) => {
+  if (!tickets.length) {
+    return (
+      <Container>
+        <Title>There are no tickets available!</Title>
+      </Container>
+    );
+  }
+
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <Button>View</Button>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <Container>
+      <TableContainer>
+        <Title>Tickets</Title>
+        <Table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{ticketList}</tbody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 };
 
-export const getServerSideProps = async context => {
-  return { props: {} };
+export const getServerSideProps = async appContenxt => {
+  const client = buildClient(appContenxt);
+  const { data } = await client.get('/api/tickets');
+
+  const tickets = data.filter(ticket => !ticket.orderId);
+
+  return { props: { tickets } };
 };
 
 export default Home;
---
+++ client/styles/pages/Home.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  padding-top: 100px;
+`;
+
+export const Title = styled.h1`
+  color: ${props => props.theme.colors.text};
+`;
+
+export const TableContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  background: ${props => props.theme.colors.background_80};
+  border-radius: 8px;
+  border: 0;
+  box-shadow: 7px 2px 14px -6px rgba(0, 0, 0, 0.75);
+  padding: 30px 0 30px 30px;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  padding-top: 10px;
+
+  thead th,
+  tbody td {
+    color: ${props => props.theme.colors.text};
+    text-align: left;
+  }
+`;
+
+export const Button = styled.button`
+  margin: 10px 0;
+`;
